4c2b882e70832502e74e4783533bd09c3e601301
Level237 <Bramslevel129@gmail.com>
2023-03-22 19:48:24 +0100

    update

--- app/Http/Middleware/Client.php
+++ app/Http/Middleware/Client.php
@@ -15,6 +15,9 @@ class Client
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if(!auth()->check() || auth()->user()->role_id !=2){
+            abort(403);
+        }
         return $next($request);
     }
 }
