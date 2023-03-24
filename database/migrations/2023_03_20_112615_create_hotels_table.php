66887c8b88b8b89b8629195f362e6aa0245320c0
Level237 <Bramslevel129@gmail.com>
2023-03-24 08:05:37 +0100

    update

--- database/migrations/2023_03_20_112615_create_hotels_table.php
+++ database/migrations/2023_03_20_112615_create_hotels_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use App\Models\User;
 return new class extends Migration
 {
     /**
@@ -14,9 +14,7 @@ return new class extends Migration
         Schema::disableForeignKeyConstraints();
         Schema::create('hotels', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('nom_hotel');
             $table->integer('nombres_etoiles');
             $table->string('ville');
             $table->foreignIdFor(User::class)
--- database/seeders/HotelSeeder.php
+++ database/seeders/HotelSeeder.php
@@ -16,8 +16,7 @@ class HotelSeeder extends Seeder
         Hotel::create([
             'id'=>1,
             'nom_hotel'=>"Onomo hotel",
-            'email'=>'OnomoHotel123@gmail.com',
-            'password'=>bcrypt('Onomo34@H'),
+            'user_id'=>2,
             'nombres_etoiles'=>4,
             'ville'=>"douala",
             "tel"=>"678390037"
@@ -27,8 +26,7 @@ class HotelSeeder extends Seeder
     Hotel::create([
         'id'=>2,
         'nom_hotel'=>"Akwa palace",
-        'email'=>'AkwaPalace45@gmail.com',
-        'password'=>bcrypt('Akwa637palace'),
+        'user_id'=>3,
         'nombres_etoiles'=>4,
         'ville'=>"douala",
         "tel"=>"667463552"
@@ -38,8 +36,7 @@ class HotelSeeder extends Seeder
 Hotel::create([
     'id'=>3,
     'nom_hotel'=>"Ibis hotel",
-    'email'=>'HotelIbis@gmail.com',
-    'password'=>bcrypt('IbisHotel45@2Z'),
+    'user_id'=>4,
     'nombres_etoiles'=>3,
     'ville'=>"douala",
     "tel"=>"678039436"
@@ -49,8 +46,7 @@ Hotel::create([
 Hotel::create([
     'id'=>4,
     'nom_hotel'=>"Hotel la falaise",
-    'email'=>'FalaiseHotel34@gmail.com',
-    'password'=>bcrypt('HotelFalaise647@'),
+    'user_id'=>5,
     'nombres_etoiles'=>4,
     'ville'=>"douala",
     "tel"=>"678039436"
@@ -60,8 +56,7 @@ Hotel::create([
 Hotel::create([
     'id'=>5,
     'nom_hotel'=>"Hotel sawa",
-    'email'=>'HotelSawa67@gmail.com',
-    'password'=>bcrypt('SawaHotel23@'),
+    'user_id'=>6,
     'nombres_etoiles'=>3,
     'ville'=>"douala",
     "tel"=>"678466374"
@@ -71,8 +66,7 @@ Hotel::create([
 Hotel::create([
     'id'=>6,
     'nom_hotel'=>"Star land",
-    'email'=>'StarLand45@gmail.com',
-    'password'=>bcrypt('StarLand637@jd'),
+    'user_id'=>7,
     'nombres_etoiles'=>5,
     'ville'=>"douala",
     "tel"=>"678637734"
@@ -82,8 +76,7 @@ Hotel::create([
 Hotel::create([
     'id'=>7,
     'nom_hotel'=>"Residence la falaisse",
-    'email'=>'ResidenceFalaise64@gmail.com',
-    'password'=>bcrypt('Falaise64735@'),
+    'user_id'=>8,
     'nombres_etoiles'=>3,
     'ville'=>"douala",
     "tel"=>"689473552"
@@ -93,8 +86,7 @@ Hotel::create([
 Hotel::create([
     'id'=>8,
     'nom_hotel'=>"Vallee des princes",
-    'email'=>'Vallee45@gmail.com',
-    'password'=>bcrypt('vallpri453@d'),
+    'user_id'=>9,
     'nombres_etoiles'=>4,
     'ville'=>"douala",
     "tel"=>"689473552"
@@ -104,8 +96,7 @@ Hotel::create([
 Hotel::create([
     'id'=>9,
     'nom_hotel'=>"Noubou internationnal",
-    'email'=>'Noubou34@gmail.com',
-    'password'=>bcrypt('noubou345@'),
+    'user_id'=>10,
     'nombres_etoiles'=>4,
     'ville'=>"douala",
     "tel"=>"689473552"
@@ -115,8 +106,7 @@ Hotel::create([
 Hotel::create([
     'id'=>10,
     'nom_hotel'=>"La falaise de yaounde",
-    'email'=>'YdeFalaise67@gmail.com',
-    'password'=>bcrypt('FalYde367'),
+    'user_id'=>11,
     'nombres_etoiles'=>4,
     'ville'=>"Yaoundé",
     "tel"=>"6902373763"
@@ -126,9 +116,8 @@ Hotel::create([
 Hotel::create([
     'id'=>11,
     'nom_hotel'=>"Safyad hotel",
-    'email'=>'hotelSafyad@gmail.com',
-    'password'=>bcrypt('safyad@23'),
-    'nombres_etoiles'=>3,
+    'user_id'=>12,
+    'nombres_etoiles'=>2,
     'ville'=>"Yaoundé",
     "tel"=>"692374893"
 ],
@@ -137,8 +126,7 @@ Hotel::create([
 Hotel::create([
     'id'=>12,
     'nom_hotel'=>"Hotel Franco",
-    'email'=>'FrancoHotel23@gmail.com',
-    'password'=>bcrypt('francoHotel56'),
+    'user_id'=>13,
     'nombres_etoiles'=>3,
     'ville'=>"Yaoundé",
     "tel"=>"695636738"
@@ -148,8 +136,7 @@ Hotel::create([
 Hotel::create([
     'id'=>13,
     'nom_hotel'=>"Hilton Hotel",
-    'email'=>'HiltonHotelYde34@gmail.com',
-    'password'=>bcrypt('hilton45@fhgh'),
+    'user_id'=>14,
     'nombres_etoiles'=>5,
     'ville'=>"Yaoundé",
     "tel"=>"695636738"
@@ -159,8 +146,7 @@ Hotel::create([
 Hotel::create([
     'id'=>14,
     'nom_hotel'=>"Boun's hotel",
-    'email'=>'BounsHotel@gmail.com',
-    'password'=>bcrypt('dueobound@djjh'),
+    'user_id'=>15,
     'nombres_etoiles'=>4,
     'ville'=>"Yaoundé",
     "tel"=>"695636738"
@@ -170,9 +156,8 @@ Hotel::create([
 Hotel::create([
     'id'=>15,
     'nom_hotel'=>"Massao palace hotel",
-    'email'=>'MassaoPalace@gmail.com',
-    'password'=>bcrypt('MassaoHJ@palace'),
-    'nombres_etoiles'=>4,
+    'user_id'=>16,
+    'nombres_etoiles'=>3,
     'ville'=>"Yaoundé",
     "tel"=>"695636738"
 ],
@@ -181,8 +166,7 @@ Hotel::create([
 Hotel::create([
     'id'=>16,
     'nom_hotel'=>"Hotel le fibi",
-    'email'=>'FibiHotel@gmail.com',
-    'password'=>bcrypt('fibiHotel452#'),
+    'user_id'=>17,
     'nombres_etoiles'=>4,
     'ville'=>"Yaoundé",
     "tel"=>"695636738"
@@ -192,8 +176,7 @@ Hotel::create([
 Hotel::create([
     'id'=>17,
     'nom_hotel'=>"Hotel la couronne RW",
-    'email'=>'CouroneHotel78@gmail.com',
-    'password'=>bcrypt('Courone723@'),
+    'user_id'=>18,
     'nombres_etoiles'=>3,
     'ville'=>"Yaoundé",
     "tel"=>"695636738"
@@ -203,8 +186,7 @@ Hotel::create([
 Hotel::create([
     'id'=>18,
     'nom_hotel'=>"Yaahot",
-    'email'=>'Yaahot78@gmail.com',
-    'password'=>bcrypt('yaahot#éjd'),
+    'user_id'=>19,
     'nombres_etoiles'=>3,
     'ville'=>"Yaoundé",
     "tel"=>"695636738"
@@ -214,8 +196,7 @@ Hotel::create([
 Hotel::create([
     'id'=>19,
     'nom_hotel'=>"Djeuga palace hotel",
-    'email'=>'Djeuga67@gmail.com',
-    'password'=>bcrypt('Djeuga23@'),
+    'user_id'=>20,
     'nombres_etoiles'=>2,
     'ville'=>"Yaoundé",
     "tel"=>"695636738"
