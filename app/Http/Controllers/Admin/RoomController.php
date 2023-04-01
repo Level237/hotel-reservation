f7db287c4ab905413f2e1947119362fda55fc9c5
level237 <Bramslevel129@gmail.com>
2023-04-01 16:54:35 +0100

    update8

--- app/Http/Controllers/Admin/RoomController.php
+++ app/Http/Controllers/Admin/RoomController.php
@@ -7,6 +7,7 @@ use App\Models\Chambre;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use App\Models\Room_Type;
 
 class RoomController extends Controller
 {
@@ -15,7 +16,7 @@ class RoomController extends Controller
      */
     public function index()
     {
-  
+
         $rooms=DB::table('chambres')
         ->join('room__types','chambres.room__type_id','room__types.id')
         ->select('chambres.id','chambres.hotel_id','chambres.image','room__types.title','room__types.price','room__types.adult_capacity','room__types.kids_capacity')
@@ -29,7 +30,9 @@ class RoomController extends Controller
      */
     public function create()
     {
-        return view('admin.room.create');
+        $room_types=Room_Type::all();
+        $hotels=Hotel::all();
+        return view('admin.room.create',compact('hotels','room_types'));
     }
 
     /**
@@ -37,7 +40,18 @@ class RoomController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $path = 'assets/images/chambres';
+            $myimage = $request->image->getClientOriginalName();
+            $pathImage="$path/$myimage";
+            $request->image->move(public_path($path), $myimage);
+        $chambre=new Chambre;
+        $chambre->room__type_id=$request->type;
+        $chambre->hotel_id=$request->hotel;
+        $chambre->image=$pathImage;
+        $chambre->status=0;
+        $chambre->save();
+
+        return to_route('admin.rooms.index')->with('success','Hotel enregistré avec success');
     }
 
     /**
@@ -67,8 +81,9 @@ class RoomController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Room_Type $type)
     {
-        //
+        $type->delete();
+        return redirect()->back()->with('danger','hotel suprimé avec success');
     }
 }
--- resources/views/admin/room/create.blade.php
+++ resources/views/admin/room/create.blade.php
@@ -8,28 +8,28 @@ Ajouter un Hotel
 @section('content')
 
 <main>
-	
+
     <!-- =======================
     Page Banner START -->
     <section class="pt-4 pt-md-5 pb-0">
         <div class="container">
             <div class="row">
                 <div class="col-12 mx-auto text-center">
-                    <h1 class="fs-2 mb-2">Nouvel Hotel</h1>
+                    <h1 class="fs-2 mb-2">Nouvelle chambre</h1>
                 </div>
             </div>
         </div>
     </section>
     <!-- =======================
     Page Banner END -->
-    
+
     <!-- =======================
     Main content START -->
     <section>
         <div class="container">
             <div class="row">
                 <div class="col-lg-10 mx-auto">
-                    <form class="vstack gap-4" action="{{ route('admin.hotels.store') }}" method="post" enctype="multipart/form-data">
+                    <form class="vstack gap-4" action="{{ route('admin.rooms.store') }}" method="post" enctype="multipart/form-data">
                         @csrf
                         <!-- Owner Detail START -->
                         <div class="card shadow">
@@ -37,94 +37,76 @@ Ajouter un Hotel
                             <div class="card-header border-bottom">
                                 <h5 class="mb-0"> Detail</h5>
                             </div>
-    
+
                             <!-- Card body -->
                             <div class="card-body">
                                 <div class="row g-3">
                                     <!-- Owner name -->
                                     <div class="col-12">
-                                        <label class="form-label">Nom Hotel</label>
-                                        <div class="input-group">
-                                            <input type="text" required name="nom_hotel" class="form-control" placeholder="Entrer le nom de l'Hotel">
-                                           
-                                        </div>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <label class="form-label">Localisation</label>
-                                        <select name="localisation" class="form-select js-choice">
-                                            <option value="Douala">Douala</option>
-                                            <option value="Yaoundé">Yaoundé</option>
-                                            <option value="Bafoussam">Bafoussam</option>
+                                        <label class="form-label">Hotel</label>
+                                        <select name="hotel" class="form-select js-choice">
+                                            @foreach ($hotels as $hotel)
+                                            <option value="{{ $hotel->id }}">{{ $hotel->nom_hotel }}</option>
+                                            @endforeach
+
+
                                         </select>
                                     </div>
-                                    <div class="col-md-6">
-                                        <label class="form-label">Nombre d'etoile</label>
-                                        <input class="form-control" name="nombres_etoiles" required type="number" placeholder="" max="5" min="1" >
-                                    </div>
-                                    <!-- Contact number -->
-                                    <div class="col-md-6">
-                                        <label class="form-label">Contact</label>
-                                        <input class="form-control" name="tel" type="text" required placeholder="Enter le numero de telephone">
-                                    </div>
-    
-                                    <!-- Email -->
-                                    <div class="col-md-6">
-                                        <label class="form-label">Adresse email</label>
-                                        <input class="form-control" name="email" type="email" required placeholder="Enter l'email">
-                                    </div>
-    
-                                    <!-- Address -->
+
                                     <div class="col-12">
-                                        <label class="form-label">Nom du Gerant</label>
-                                        <input type="text" name="name" class="form-control" required placeholder="Entrer le nom du Gerant de l'Hotel">
+                                        <label class="form-label">Selectionnez la classe de chambre</label>
+                                        <select name="type" class="form-select js-choice">
+                                            @foreach ($room_types as $type)
+                                            <option value="{{ $type->id }}">{{ $type->title }}</option>
+                                            @endforeach
+
+
+                                        </select>
                                     </div>
-                                    <div class="col-12">
-                                        <label class="form-label">Mot de passe de l'hotel</label>
-                                        <input type="password" name="password" class="form-control" required placeholder="Entrer le mot de passe">
-                                    </div>
-                        
-                                    <!-- City -->
-                                    
+
+
+
+
                                 </div>
                             </div>
                         </div>
                         <!-- Owner Detail END -->
-    
+
                         <!-- Cab Detail START -->
                         <div class="card shadow">
                             <!-- Card header -->
                             <div class="card-header border-bottom">
                                 <h5 class="mb-0">Media</h5>
                             </div>
-    
+
                             <!-- Card body -->
                             <div class="card-body">
                                 <div class="row g-3">
                                     <!-- Car name -->
-                                 
-    
-                                  
+
+
+
                                     <!-- Image Gallery -->
                                     <div class="col-12">
-                                        <label class="form-label">Image d'hotel *</label>
+                                        <label class="form-label">Image de la Chambre*</label>
                                         <div class="dropzone dropzone-custom" data-dropzone='{"maxFiles": 5, "addRemoveLinks": false}'>
                                             <div class="dz-message needsclick">
-                                                
-                                                
+
+
                                             </div>
                                             <input type="file" name="image" required>
-                                        </div> 
+                                        </div>
                                         <p class="small mb-0 mt-2"><b>Note:</b>Uniquement JPG, JPEG et PNG. Les dimensions suggérées sont 600px * 450px. L'image la plus grande sera recadrée au format 4:3 pour s'adapter à nos vignettes/présentations..</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <!-- Cab Detail END -->
-    
+
                         <!-- Driver Detail START -->
-                      
+
                         <!-- Driver Detail END -->
-    
+
                         <!-- Button -->
                         <div class="text-end">
                             <button type="submit" class="btn btn-primary mb-0">Ajouter</button>
@@ -136,7 +118,7 @@ Ajouter un Hotel
     </section>
     <!-- =======================
     Main content END -->
-    
+
     </main>
     <script src="{{ asset('assets/vendor/choices/js/choices.min.js') }}"></script>
     <script src="{{ asset('assets/vendor/dropzone/js/dropzone.js') }}"></script>
